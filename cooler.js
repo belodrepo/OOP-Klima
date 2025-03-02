e480aeb5c0812abdeb3bd02fd8f188f3e7f8e7a8
belodrepo <sxbravo@gmail.com>
2025-03-02 16:51:13 +0100

    Fűtési teljesítmény számítás hozzáadása

--- cooler.js
+++ cooler.js
@@ -7,15 +7,24 @@ class Cooler {
         this.height = h;
         this.category = c;
     }
+
+    coolarr = [25, 30, 40];
+    heatarr = [40, 60, 80];
+
     classification = [
-        { class: 1, cat: "hőszigetelt",  power: 25},
-        { class: 2, cat: "közepesen hőszigetelt", power: 30},
-        { class: 3, cat: "szigeteletlen", power: 40}
+        { class: 1, cat: "hőszigetelt",  power: 0},
+        { class: 2, cat: "közepesen hőszigetelt", power: 0},
+        { class: 3, cat: "szigeteletlen", power: 0}
      ];
 
     getRoomVolume = () => this.width * this.length * this.height;
 
     getCoolingPower = () => {
+
+        this.coolarr.forEach((elem, i) => {
+            this.classification[i].power = elem;
+        });
+
         let vol = this.getRoomVolume();
         let coolingPower = 0;
 
@@ -32,6 +41,29 @@ class Cooler {
         }
         return coolingPower;
     }
+    getHeatingPower = () => {
+
+        this.heatarr.forEach((elem, i) => {
+            this.classification[i].power = elem;
+        });
+
+        let vol = this.getRoomVolume();
+        let heatingPower = 0;
+
+        switch(this.category) {
+            case 1:
+            heatingPower = vol * this.classification[0].power;
+            break;
+            case 2:
+            heatingPower = vol * this.classification[1].power;
+            break;
+            case 3:
+            heatingPower = vol * this.classification[2].power;
+            break;
+        }
+        return heatingPower;
+    }
+
     getInsulationCategory = () => {
         let insulation = "";
         switch(this.category) {
@@ -49,7 +81,8 @@ class Cooler {
     }
 }
 
-const myCooler = new Cooler(5, 6, 2.3, 3);
-console.log(`Légköbméter: ${myCooler.getRoomVolume()} m3`);
-console.log(`Hűtási teljesítmény: ${myCooler.getCoolingPower()} W`);
-console.log(`Besorolás: ${myCooler.getInsulationCategory()}`);
+const myCooler = new Cooler(5, 4, 2.3, 3);
+console.log(`Számított légköbméter: ${myCooler.getRoomVolume()} m3`);
+console.log(`Szigetelési besorolás: ${myCooler.getInsulationCategory()}`);
+console.log(`Szükséges hűtési teljesítmény igény: ${myCooler.getCoolingPower() / 1000} kW`);
+console.log(`Szükséges fűtési teljesítmény igény: ${myCooler.getHeatingPower() / 1000} kW`);
---
+++ test.js
@@ -0,0 +1,6 @@
+const coolarr = [25, 30, 40];
+
+coolarr.forEach((elem, i) => {
+    console.log(elem);
+    console.log(i);
+});
